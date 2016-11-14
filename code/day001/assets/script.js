0f39f198c42891bbadf34195273bae4fbd053909
Gabriel <gabriel@multiverso.me>
2016-11-14 13:17:29 -0200

    DAY001 - Sums and subtractions working fine; Operator precedence not working yet

---
+++ code/day001/assets/script.js
@@ -0,0 +1,77 @@
+(function(){
+  window.calculator = calculator();
+  function calculator() {
+    var expression = [];
+
+    return {
+      addElement: addElement,
+      calculate: calculate,
+      clear: clear
+    };
+
+    function addElement(element) {
+      var previous;
+      previous = typeof expression[expression.length - 1] === 'undefined';
+      previous = previous ? '' : expression[expression.length - 1];
+
+      // if previous element is empty or current and previous elements
+      // are both operators, don't add the element
+      if (isInt(previous) || isInt(element)) {
+        expression.push(element);
+        refreshDisplay();
+      }
+    }
+
+    function calculate() {
+      var processed = 0;
+      // expression to string
+      expression = expression.join('');
+
+      var numbers = expression.match(/(?:[\d\.]+)+/g);
+      var operators = expression.match(/[-\+\*\/]+/g);
+
+      processed = parseInt(numbers.pop());
+      while (numbers.length > 0) {
+        var value = parseInt(numbers.pop());
+        var operator = operators.pop();
+        // var next = parseInt(numbers.pop());
+        // next = isInt(next) ? next : 0;
+
+        // console.log(value, operator, next);
+
+        switch (operator) {
+          case '+':
+            processed += value;
+            break;
+          case '-':
+            processed -= value;
+            break;
+          case '*':
+            processed *= value;
+            break;
+          case '/':
+            processed = value / processed;
+            break;
+          default:
+            break;
+        }
+      }
+      expression = [processed];
+      refreshDisplay();
+    }
+
+    function refreshDisplay() {
+      var display = document.getElementById('display');
+      display.innerHTML = expression.join('');
+    }
+
+    function clear() {
+      expression = [];
+      refreshDisplay();
+    }
+
+    function isInt(n){
+      return parseInt(n) % 1 === 0;
+    }
+  }
+})();
