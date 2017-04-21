47c50c6ce48ca13d41cb41b96fed86e9f343166f
Gabriel <gabriel@multiverso.me>
2017-04-20 23:52:38 -0300

    Update portfolio code

--- portfolio/scripts.js
+++ portfolio/scripts.js
@@ -2,19 +2,17 @@ $(document).ready(function () {
   $('body').scrollspy({ target: '#navbar', offset: 100 });
   $('.navbar-nav a').on('click', function (e) {
     e.preventDefault();
-    $(document).off("scroll");
-    $('a').each(function () {
-      $(this).removeClass('active');
-    })
-    $(this).addClass('active');
-    var target = this.hash,
-    menu = target;
-    $target = $(target);
-    $('html, body').stop().animate({
+    if (!$(this).hasClass('active')) {
+
+      $('.active').removeClass('active');
+      $(this).addClass('active');
+
+      var $target = $(this.hash);
+      $('html, body')
+      .stop()
+      .animate({
       'scrollTop': $target.offset().top-50
-    }, 500, 'swing', function () {
-      return false;
-    });
-    return false;
+      }, 500, 'swing');
+    }
   });
 });
